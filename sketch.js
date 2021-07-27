495ee9cb20c5479e2fb2b7740b5d2e2339befed9
Gyadav1718 <86192597+Gyadav1718@users.noreply.github.com>
2021-07-27 18:50:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var wall,Bullet,Thickness;
+var speed,weight;
+var bgimage;
+var recipe;
+function preload(){
+  bgiamge= loadImage ("re.jpg");
+}
+function setup() {
+  createCanvas(1000,600);
+
+}
+
+function draw() {
+  background(bgiamge) 
+
+  
+}
